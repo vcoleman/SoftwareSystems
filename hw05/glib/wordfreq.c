ed9142c9bc8f87eaa92e3c893909797c7f09f0f9
vcoleman <victoria.coleman@students.olin.edu>
2014-04-07 21:10:05 -0400

    hw05 now has comments

--- hw05/glib/wordfreq.c
+++ hw05/glib/wordfreq.c
@@ -1,9 +1,18 @@
+/*Hw05
+
+Created by: Victoria Coleman
+
+*/
+
 #include <stdio.h>
 #include <glib.h>
 #include <stdlib.h>
 #include <string.h>
 
 
+/*
+Appends a character to the end of a character array
+*/
 void append(char* s, char c)
 {
         int len = strlen(s);
@@ -11,13 +20,52 @@ void append(char* s, char c)
         s[len+1] = '\0';
 }
 
-void user_func(char *word, uint *count)
+/*
+Prints the valules and keys of a GHashTable (helper function)
+*/  
+void print_key_value(char *word, uint *count)
   {   
-
-  	//gchar *words = (gchar)words ;
-    //gint *count = (gint*) count ;
     g_print("count: %u    word : %s\n", *count, word) ;
   }
+
+/*
+Prints the valules and keys of a GHashTable
+*/  
+void print_hash_table(GHashTable *hashTable){
+    g_hash_table_foreach (hashTable, (GHFunc)print_key_value, NULL) ;  
+}
+
+/*Inserts a new value in the hashtable initialized with a frequency of one,
+or increments the frequency of the value if already in the table
+
+GHashTable hashTable: hashtable of key: word (GString), value: freq (gint)
+*/
+void add_to_hash_table(GHashTable *hashTable,gchar* g_string_new){
+
+    gint *word_number = g_new0(int,1);
+
+    word_number = g_hash_table_lookup(hashTable, g_string_new);
+
+    if (word_number != NULL){     //if key exists in hash table
+
+        gint *word_number_new = g_new(int,1);
+
+        *word_number_new = (*word_number + 1); //Increment word_number
+        g_hash_table_insert(hashTable, g_string_new, (gpointer *)(word_number_new));
+        
+        g_free(word_number);
+
+    }
+    else{ //put key in hash table and make value 1
+        gint *one = g_new(int,1);
+        *one = 1;
+        g_hash_table_insert(hashTable, g_string_new, (gpointer *)one);
+    }
+}
+
+/* Creates a GHashTable of all the words in the "AfricanMillionare.txt" file
+and prints out the words and their frequencies
+*/
 
 int main ()
 {
@@ -36,60 +84,35 @@ int main ()
     else
     {
         int found_word = 0;
-        char str[256];
-        //gint *word_number = g_new(int,1);
+        char word_buffer[256];  //word string
 
+        //Create hashtable to store word & frequency
         GHashTable *hashTable = g_hash_table_new(g_int_hash, g_int_equal);
 
+        //If tile does not error
         while ((c =fgetc(input_file)) != EOF )
         {
-
-            //if it's an alpha, convert it to lower case
+            //if it's an alpha, convert it to lower case and add to word buffer
             if (isalpha(c))
             {
                 found_word = 1;
                 c = tolower(c);
-                append(str, c);
+                append(word_buffer, c);
             }
             else {
-                if (found_word & c == ' ' | c == '\n') { /////////////wahoooo!
-                    gchar* g_string_new = g_strdup(str);
-                    gint *word_number = g_new0(int,1);
-                    
-                    word_number = g_hash_table_lookup(hashTable, g_string_new);
+                //Check to see if it is a new word
+                if (found_word & (c == ' ' | c == '\n')) {
 
-                    if (word_number != NULL){
-                    	gint *one = g_new(int,1);
-                    	*one = 1;
-                        gint *word_number_new = g_new(int,1);
-                        *word_number_new = (*word_number + *one);
+                    gchar* g_string_new = g_strdup(word_buffer);
 
-                        //g_print("word_numer: %d \n", *word_number);
-
-                    	g_hash_table_insert(hashTable, g_string_new, (gpointer *)(word_number_new));
-                    	g_free(one);
-                        g_free(word_number);
-
-                    }
-                    else{
-                    	gint *one = g_new(int,1);
-                    	*one = 1;
-                    	g_hash_table_insert(hashTable, g_string_new, (gpointer *)one);
-                    }
+                    add_to_hash_table(hashTable, g_string_new);
 
                     found_word=0;
-                    str[0] = '\0';
+                    word_buffer[0] = '\0';  //clears word buffer for new word 
                 }
             }
-
         }
- 
-  		g_hash_table_foreach (hashTable, (GHFunc)user_func, g_string_new) ;  
+        print_hash_table(hashTable);
     }
-
     fclose(input_file);
-
-    printf("\n");
-
-    return 0;
 }
